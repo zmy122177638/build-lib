4cd0782942ee1e13a61e7d5ad92496f0af2bdac2
Anles <122177638@qq.com>
2020-10-26 18:24:39 +0800

    chore: gitignore lib

--- lib/types/index.d.ts
+++
@@ -1,3 +0,0 @@
-/** 导出模块所有方法 */
-export * from './modules/format';
-export declare function testBuildTarget(): () => string;
--- lib/types/modules/format.d.ts
+++
@@ -1,4 +0,0 @@
-/** 时间格式化 */
-export declare function formatDate(date: any, format?: string): string;
-/** 金钱格式化 */
-export declare function formatNumber(value: number | string, precision?: number, seperator?: string): string | 0;
